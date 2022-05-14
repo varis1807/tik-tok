1d03c69caa9d4cc0520c8376bc829283d9df6ad6
Varis Rana <vrvarisrana123@gmail.com>
2022-05-14 06:15:53 +0530

    commit 14

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+      apiKey: "AIzaSyAsj4xqbzjXt4pxK8BU_pFk8yUhl6_m3dU",
+      authDomain: "tik-tok-6233e.firebaseapp.com",
+      projectId: "tik-tok-6233e",
+      storageBucket: "tik-tok-6233e.appspot.com",
+      messagingSenderId: "372581035951",
+      appId: "1:372581035951:web:a4d75a1c0017137e03495a"
+    };
